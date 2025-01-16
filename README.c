40c32e0a7327c07976f31a888cb418df040f411f
Jokimpr <jokimprathap5@gmail.com>
2025-01-16 09:36:06 +0530

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,17 @@
+# demo1
+#include<iostream.h>
+#include<stdio.h>
+#include<conio.h>
+void main()
+  {
+          int num1,num2,c;
+          clrscr();
+          printf("enter the value of first number:");
+          scanf("%d, &num1");
+          printf("enter the value of second number:");
+          scanf("%d, &num2");
+          c=a+b;
+          printf("the value of c is:",c);
+          getch();
+  }      
+          
